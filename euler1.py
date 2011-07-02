1c517753f39bc32d4e7f30f6828bd7e22f13b4f2
Akshar Raaj <akshar@agiliq.com>
2011-07-02 23:35:51 +0530

    added a print statement to euler1.py

--- euler1.py
+++ euler1.py
@@ -5,3 +5,4 @@ for i in range(3,1000):
     if(i%3==0 or i%5==0):
         sum+=i
 print ("Sum of natural numbers below 1000 which are multiple of either 3 or 5 is is {0}".format(sum))
+print "Complete",
