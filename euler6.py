3db438205ce24aef16e0ca622438848d2d17833a
Shabda Raaj <shabda.raaj@gmail.com>
2011-07-02 22:53:46 +0530

    Some pep-8 changes for p6

--- euler6.py
+++ euler6.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
-#filename euler6.py
-def sumOfSquares(number):
+
+def sum_of_squares(number):
     sum=0
     for i in range(1,number+1):
         square=i**2
         sum=sum+square
     return sum
 
-def squareOfSum(number):
+def square_of_sum(number):
     sum=0
     for i in range(1,number+1):
         sum=sum+i
     return sum**2
 
-sumOfSqure=sumOfSquares(100)
-squareOfSum=squareOfSum(100)
-print ("Difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is {0}".format(squareOfSum-sumOfSqure))
+sum_of_squre=sum_of_squares(100)
+square_of_sum=square_of_sum(100)
+print ("Difference between the sum of the squares of the first one hundred "
+       "natural numbers and the square of the sum is "
+       "{0}".format(square_of_sum-sum_of_squre))
