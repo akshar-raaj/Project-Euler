a86ac23d6f8a8140e33d5bc18e2c6d3243b44443
Shabda Raaj <shabda.raaj@gmail.com>
2011-07-03 22:12:22 +0530

    Some pep-8 changes

--- euler10.py
+++ euler10.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-def isPrime(number):
+def is_prime(number):
     start=2
     end=number**0.5
     end=int(end)
@@ -13,12 +13,12 @@ def isPrime(number):
     return True
 
 def sum_primes_below(number):
-    i=3 #We start testing prime numbers starting from 3
-    sum=2 #Since we did not test 2, we have included it in the sum
+    i=3#We start testing prime numbers starting from 3
+    primes_sum = 2
     while(i<=number):
-        if(isPrime(i)):
-            sum=sum+i
+        if(is_prime(i)):
+            primes_sum=primes_sum+i
         i=i+2 #We know that an even number cannot be prime. So, we increase it in steps of 2
-    print ("Sum of all the primes below two million is "+str(sum))
+    print ("Sum of all the primes below two million is "+str(primes_sum))
 
 sum_primes_below(2000000)
