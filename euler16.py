a619402622ff2702e111a25059e969dc1a9e619c
Akshar Raaj <akshar@agiliq.com>
2011-07-04 01:17:07 +0530

    Sum of the digits in 2 power 1000

---
+++ euler16.py
@@ -0,0 +1,9 @@
+thousand_power_of_two=2**1000
+print thousand_power_of_two
+thousand_power_of_two=str(thousand_power_of_two)
+required_sum=0
+for each in thousand_power_of_two:
+    required_sum+=int(each)
+print "Sum of the digits of the number 2 power 1000 is "+str(required_sum)
+    
+    
