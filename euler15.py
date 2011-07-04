86cdd7998445bef58a58b572c6cd689ef7a2bdaf
Akshar Raaj <akshar@agiliq.com>
2011-07-04 12:22:09 +0530

    Possible routes in a 20 by 20 grid

---
+++ euler15.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""Logic
+   The path consists of 20 steps down and 20 steps right. So, its is of the form DDDRRR......DDRR.
+   Hence it consists of 20 D and 20 R. So, the number of such permutations possible is 40!/(20!*20!)"""
+def find_factorial(number):
+    factorial=1
+    for i in range(1,number+1):
+        factorial*=i
+    return factorial
+
+factorial_fourty=find_factorial(40) 
+factorial_twenty=find_factorial(20)
+number_of_paths=factorial_fourty/(factorial_twenty*factorial_twenty)
+print "Number of possible routes from top left corner to bottom right corner in a 20 by 20 grid is "+str(number_of_paths)
