33021ecb7801d9d87a64607d8b93e0de72c0e358
Akshar Raaj <akshar@agiliq.com>
2012-02-04 18:27:54 +0530

    Confirm to pep8 guidelines

--- euler5.py
+++ euler5.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 #filename euler5.py
+
+
 def findLCMofNumbersUptoThis():
-    step=0
-    shouldContinue=True
+    step = 0
+    shouldContinue = True
     while(shouldContinue):
-        step=step+20
-        if(step%11==0 and step%12==0 and step%13==0 and step%14==0 and step%15==0 and step%16==0 and step%17==0 and step%18==0 and step%19==0 and step%20==0):
-            shouldContinue=False
-    print ("Smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is {0}".format(step))
+        step = step + 20
+        if(step % 11 == 0 and step % 12 == 0 and
+            step % 13 == 0 and step % 14 == 0 and step % 15 == 0
+            and step % 16 == 0 and step % 17 == 0 and step % 18 == 0
+            and step % 19 == 0 and step % 20 == 0):
+                shouldContinue = False
+    print "LCM of 1 to 20 is %d" % step
 findLCMofNumbersUptoThis()
-                
-            
--- euler7.py
+++ euler7.py
@@ -1,23 +1,24 @@
 def is_prime(number):
-    lowest=2
-    highest=number**0.5
-    highest=int(highest)
-    while(lowest<=highest):
-        if(number%lowest==0):
+    lowest = 2
+    highest = number ** 0.5
+    highest = int(highest)
+    while lowest <= highest:
+        if number % lowest == 0:
             return False
-        lowest=lowest+1
+        lowest = lowest + 1
     return True
 
+
 def generate_prime_numbers():
-    primeCount=0
-    numberToBeChecked=1
-    requiredPrime=1
-    while(primeCount<10001):
-        numberToBeChecked=numberToBeChecked+1
-        if(is_prime(numberToBeChecked)):
-            requiredPrime=numberToBeChecked
-            primeCount=primeCount+1
+    primeCount = 0
+    numberToBeChecked = 1
+    requiredPrime = 1
+    while primeCount < 10001:
+        numberToBeChecked = numberToBeChecked + 1
+        if is_prime(numberToBeChecked):
+            requiredPrime = numberToBeChecked
+            primeCount = primeCount + 1
     print ("10001st prime number is {0}".format(requiredPrime))
-    
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     generate_prime_numbers()
