4f3c3bf820e0957368a06835d69960d0639b22ec
Akshar Raaj <akshar@agiliq.com>
2012-02-04 23:35:34 +0530

    confirm to pep8 guideleines for euler1 to euler10

--- euler10.py
+++ euler10.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python
 
+
 def is_prime(number):
-    start=2
-    end=number**0.5
-    end=int(end)
-    while(start<=end):
-        if(number%start==0):
+    start = 2
+    end = number ** 0.5
+    end = int(end)
+    while start <= end:
+        if(number % start == 0):
             #print start
             return False
-        start=start+1
+        start = start + 1
     #print number
     return True
 
+
 def sum_primes_below(number):
-    i=3#We start testing prime numbers starting from 3
+    i = 3  # We start testing prime numbers starting from 3
     primes_sum = 2
-    while(i<=number):
+    while(i <= number):
         if(is_prime(i)):
-            primes_sum=primes_sum+i
-        i=i+2 #We know that an even number cannot be prime. So, we increase it in steps of 2
-    print ("Sum of all the primes below two million is "+str(primes_sum))
+            primes_sum = primes_sum + i
+	"We know that an even number cannot \
+	be prime. So, we increase it in steps of 2"
+	i = i + 2
+    print ("Sum of all the primes below two million is " + str(primes_sum))
 
 sum_primes_below(2000000)
--- euler9.py
+++ euler9.py
@@ -1,24 +1,29 @@
-limit=1000
-def is_pythagorean_triplet(a,b,c):
-    if(a**2+b**2==c**2):
+limit = 1000
+
+
+def is_pythagorean_triplet(a, b, c):
+    if(a ** 2 + b ** 2 == c ** 2):
         return True
     return False
 
-def is_sum_thousand(a,b,c):
-    if(a+b+c==1000):
+
+def is_sum_thousand(a, b, c):
+    if(a + b + c == 1000):
         return True
     return False
+
 
 def find_required_pythagorean_triplet():
-    for a in range(1,1000):
-        for b in range(a+1,1000):
-            c=1000-(a+b)
-            if(is_pythagorean_triplet(a,b,c)):
+    for a in range(1, 1000):
+        for b in range(a + 1, 1000):
+            c = 1000 - (a + b)
+            if(is_pythagorean_triplet(a, b, c)):
             #if((a**2+b**2==c**2) and (a+b+c==1000)):
-                required_number=a*b*c
-                print ("Product abc for only pythagorean triplet for which a+b+c is 1000 is {0}".format(required_number))
+                required_number = a * b * c
+                print ("Product abc for only pythagorean triplet \
+for which a+b+c is 1000 is {0}".format(required_number))
                 return
-                
+
 find_required_pythagorean_triplet()
 #print is_pythagorean_triplet(3,4,7)
 #print is_sum_thousand(100,200,600)
