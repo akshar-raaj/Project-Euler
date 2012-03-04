497656fdee9cdc517a3a37589c1d16a17fce109c
Akshar Raaj <akshar@agiliq.com>
2012-03-04 19:47:48 +0530

    euler 13 made pythonic, conforms to pep8 and pyflakes

--- euler13.py
+++ euler13.py
@@ -1,9 +1,5 @@
-data_file=open("euler13_data")
-string_data=data_file.read()
-list_of_string_representation_of_number=string_data.split("\n")
-del list_of_string_representation_of_number[len(list_of_string_representation_of_number)-1]
-numbers_sum=0
-for string_rep_each in list_of_string_representation_of_number:
-    numbers_sum=numbers_sum+int(string_rep_each)
-numbers_sum=str(numbers_sum)
-print "First ten digits of the sum of the given one-hundred 50-digit numbers is "+numbers_sum[:10]
+data_file = open('euler13_data')
+numbers_sum = sum([int(each_line) for each_line in data_file])
+print "First ten digits of the sum of the given one hundred \
+50-digits numbers is", str(numbers_sum)[:10]
+data_file.close()
