63f393e9b66e00b0a9e0c0a8ecc6b8434c5a9d66
Akshar Raaj <akshar@agiliq.com>
2012-03-04 22:27:19 +0530

    euler20.py made pythonic and conforms to pep8 and pyflakes

--- euler20.py
+++ euler20.py
@@ -1,9 +1,3 @@
-product=1
-for i in range(1,101):
-    product*=i
-#print product
-product=str(product)
-digits_sum=0
-for each in product:
-    digits_sum+=int(each)
-print "Sum of digits in 100! is "+str(digits_sum)
+product = reduce(lambda x, y: x * y, range(1, 101))
+digits_sum = sum([int(each) for each in str(product)])
+print "Sum of digits in 100! is", digits_sum
