d0421bb28f3aeac3a6c8f0dfbd891763bf7f5983
Akshar Raaj <akshar@agiliq.com>
2012-03-04 22:19:07 +0530

    euler16 made pythonic and conforms to pep8 and pyflakes

--- euler16.py
+++ euler16.py
@@ -1,9 +1,3 @@
-thousand_power_of_two=2**1000
-print thousand_power_of_two
-thousand_power_of_two=str(thousand_power_of_two)
-required_sum=0
-for each in thousand_power_of_two:
-    required_sum+=int(each)
-print "Sum of the digits of the number 2 power 1000 is "+str(required_sum)
-    
-    
+thousand_power_of_two = 2 ** 1000
+sum_of_digits = sum([int(each) for each in str(thousand_power_of_two)])
+print "Sum of the digits of the required number", sum_of_digits
