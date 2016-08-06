1bcb58f11d1ee9fc599a8558e29eb24968d48d95
Akshar Raaj <akshar@agiliq.com>
2016-08-06 12:57:40 +0530

    Generators are more memory efficient than list

--- euler1.py
+++ euler1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python -tt
-numbers = [num for num in range(3, 1000) if num % 3 == 0 or num % 5 == 0]
-the_sum = sum(numbers)
+the_sum = sum(num for num in xrange(3, 1000) if num % 3 == 0 or num % 5 == 0)
 print """Sum of natural numbers below 1000 which are multiple \
 of either 3 or 5 is is %d""" % (the_sum,)
