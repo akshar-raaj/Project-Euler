8ccccd1348650d3a22a379ab1cb8c21bb38ff984
Akshar Raaj <akshar@agiliq.com>
2016-08-06 15:36:07 +0530

    If inside for loop can be replaces by using all() builtin

--- euler3.py
+++ euler3.py
@@ -1,31 +1,10 @@
 #!/usr/bin/python
 #filename euler3.py
 def isPrime(number):
-    sqrtOfNumber=number**0.5
-    sqrtOfNumber=int(sqrtOfNumber)
-    #print sqrtOfNumber
-    for i in range(2,sqrtOfNumber+1):
-        if(number%i==0):
-            return False
-    return True
+    sqrtOfNumber = int(number**0.5)
+    return all(bool(number % i) for i in xrange(2, sqrtOfNumber + 1))
 
-"""def findPrimeFactors(number):
-    maxToCheck=number/2
-    maxToCheck=int(maxToCheck)
-    isFactorPrime=False
-    highestPrimeFactor='a'
-    #for i in range(2,maxToCheck+1):
-    i=2
-    while(i<maxToCheck+1):
-        if(number%i==0):
-            isFactorPrime=isPrime(i)
-            if(isFactorPrime):
-                highestPrimeFactor=i
-        i=i+1
-    print highestPrimeFactor"""
-    
 def findLowestPrimeNumberBetween(lowerLimit,upperLimit):
-    #for i in range(lowerLimit,upperLimit+1):
     i=lowerLimit
     while(i<=upperLimit):
         if(isPrime(i)):
@@ -35,18 +14,28 @@ def findLowestPrimeNumberBetween(lowerLimit,upperLimit):
 
 def factorize(number):
     quotient=number
-    lowestPrimeFactor=2
-    lowerLimitForPrimeFactorPrimeFactor=lowestPrimeFactor
+    lowerLimitForPrimeFactor=2
     primeFactorsList=[]
     while(quotient!=1):
-        primeFactor=findLowestPrimeNumberBetween(lowerLimitForPrimeFactorPrimeFactor,quotient)
+        primeFactor=findLowestPrimeNumberBetween(lowerLimitForPrimeFactor,quotient)
         if(quotient%primeFactor==0):
             quotient=quotient/primeFactor
             primeFactorsList.append(primeFactor)
             #print primeFactor
         else:
-            lowerLimitForPrimeFactorPrimeFactor=lowerLimitForPrimeFactorPrimeFactor+1
+            lowerLimitForPrimeFactor=lowerLimitForPrimeFactor+1
     print ("Largest prime factor of {0} is {1}".format(number,primeFactorsList[len(primeFactorsList)-1]))       
 
 factorize(600851475143)
+
+#print isPrime(4)
+#print isPrime(8)
+#print isPrime(11)
+#print isPrime(12)
+#print isPrime(13)
+#print isPrime(14)
+#print isPrime(51)
+#print isPrime(53)
+#print isPrime(4)
+
             
